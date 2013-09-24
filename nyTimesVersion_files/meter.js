df665bcccbaf1d5c9f40e79286a36fe5eb80fa16
Mihir Zaveri <mihir.a.zaveri@gmail.com>
2013-09-24 00:25:54 -0700

    first publish

---
+++ nyTimesVersion_files/meter.js
@@ -0,0 +1,1 @@
+u1379975887205({"hitPaywall":false,"counted":false,"loggedIn":true,"hash":"B156CE61A08D90348204C7860711F34B","gateway":true,"meter":true,"isCookieValid":true,"hitRegiwall":false,"regiwall":false});
